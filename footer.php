0ad80795c3739a7e435be51d514fe3d57df37357
Alice <alicewang54@gmail.com>
2015-02-22 01:51:41 -0600

    footer contact icons are not displaying

--- footer.php
+++ footer.php
@@ -6,10 +6,10 @@
                 }
             ?> class="menuLink contactLink"><a href="contact.php">CONTACT</a></span>
             <div class="socialMedia">
-                <a href="https://www.facebook.com/Alice054"><i class="icon-facebook-sign"></i></a>
-                <a href="https://twitter.com/TheGoldenMages"><i class="icon-twitter-sign"></i></a>
-                <a href="https://plus.google.com/u/0/110431832675332798244"><i class="icon-google-plus-sign"></i></a>
-                <a href="https://github.com/Ahris"><i class="icon-github"></i></a>
+                <a href="https://www.facebook.com/Alice054"><i class="fa icon-facebook-sign"></i></a>
+                <a href="https://twitter.com/ahrisalice"><i class="fa icon-twitter-sign"></i></a>
+                <a href="https://plus.google.com/u/0/110431832675332798244"><i class="fa icon-google-plus-sign"></i></a>
+                <a href="https://github.com/Ahris"><i class="fa icon-github"></i></a>
             </div>
             <hr />
         </div>
